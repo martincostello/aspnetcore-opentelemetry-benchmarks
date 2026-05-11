cde5291a976779f038c8e3ae1f8780fb7cb097c5
Martin Costello <martin@martincostello.com>
2026-05-11 21:51:34 +0100

    Enable ASP.NET Core feature switch (#84)
    Disable `Microsoft.AspNetCore.Hosting.SuppressActivityOpenTelemetryData` as the feature that depends on it should now be available.

--- perf/Benchmarks/AspNetCoreBenchmarks.cs
+++ perf/Benchmarks/AspNetCoreBenchmarks.cs
@@ -30,8 +30,7 @@ public class AspNetCoreBenchmarks : Benchmarks, IScenario
 
     protected override async Task StartServer(TelemetryConfiguration configuration)
     {
-        // TODO Depends on https://github.com/open-telemetry/opentelemetry-dotnet-contrib/pull/4376
-        ////AppContext.SetSwitch("Microsoft.AspNetCore.Hosting.SuppressActivityOpenTelemetryData", false);
+        AppContext.SetSwitch("Microsoft.AspNetCore.Hosting.SuppressActivityOpenTelemetryData", false);
         await base.StartServer(configuration);
     }
 }
